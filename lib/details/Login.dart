ed83334020b8ae7abc9ab8972bf746d1a0000fd2
FranzGwapo <franzcasimir.ondiano@gmail.com>
2023-12-30 16:43:16 +0800

    Pwans

--- lib/details/Login.dart
+++ lib/details/Login.dart
@@ -1,10 +1,21 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:mobdev_final/details/Signup.dart';
 import 'package:mobdev_final/details/Dashboard.dart';
+import 'package:mobdev_final/firebase_options.dart';
 import 'package:mobdev_final/models/StorageItem.dart';
 import 'package:mobdev_final/routes.dart';
 import 'package:mobdev_final/services/StorageService.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+}
 
 class Login extends StatefulWidget {
   static const String routeName = "login";
@@ -22,10 +33,11 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(244, 238, 237, 1.0),
       body: Center(
         child: Container(
           width: 300.0,
-          height: 300.0,
+          height: 350.0,
           padding: EdgeInsets.all(16.0),
           decoration: BoxDecoration(
             color: Colors.grey,
@@ -54,16 +66,21 @@ class _LoginState extends State<Login> {
                 obscureText: true,
               ),
               SizedBox(height: 32.0),
-              ElevatedButton(
-                onPressed: () {
-                  signIn(context, _emailController.value.text,
-                      _passwordController.value.text);
-                },
-                child: Text('Login'),
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text('Sign-in with Google'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  TextButton(
+                      onPressed: () {
+                        signIn(context, _emailController.value.text,
+                            _passwordController.value.text);
+                      },
+                      child: Text('Login')),
+                  TextButton(
+                      onPressed: () {
+                        loginWithGoogle();
+                      },
+                      child: Text('Sign-Up with Google')),
+                ],
               ),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
@@ -100,6 +117,27 @@ class _LoginState extends State<Login> {
       Navigator.pushReplacementNamed(context, Dashboard.routeName);
     } on FirebaseAuthException catch (e) {
       print(e.message);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  loginWithGoogle() async {
+    try {
+      final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+      final GoogleSignInAuthentication? googleAuth =
+          await googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth?.accessToken,
+        idToken: googleAuth?.idToken,
+      );
+
+      UserCredential? userCredential =
+          await FirebaseAuth.instance.signInWithCredential(credential);
+
+      Navigator.pushReplacementNamed(context, Dashboard.routeName);
     } catch (e) {
       print(e);
     }
